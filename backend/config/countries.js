84c55c1cc56a58aa3302650b72d55aeed46d43ca
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2026-06-12 19:50:25 +0000

    fix: Synchronize all 16 supported countries in backend config

--- backend/config/countries.js
+++ backend/config/countries.js
@@ -1,15 +1,21 @@
 // backend/config/countries.js
 const countries = [
 	{ code: "SN", name: "Sénégal", flag: "🇸🇳" },
-	{ code: "CM", name: "Cameroun", flag: "🇨🇲" },
+	{ code: "CG", name: "Congo", flag: "🇨🇬" },
 	{ code: "CI", name: "Côte d'Ivoire", flag: "🇨🇮" },
-	{ code: "ML", name: "Mali", flag: "🇲🇱" },
-	{ code: "TG", name: "Togo", flag: "🇹🇬" },
-	{ code: "BJ", name: "Bénin", flag: "🇧🇯" },
 	{ code: "BF", name: "Burkina Faso", flag: "🇧🇫" },
+	{ code: "ML", name: "Mali", flag: "🇲🇱" },
+	{ code: "NE", name: "Niger", flag: "🇳🇪" },
+	{ code: "CM", name: "Cameroun", flag: "🇨🇲" },
+	{ code: "BJ", name: "Bénin", flag: "🇧🇯" },
+	{ code: "TG", name: "Togo", flag: "🇹🇬" },
 	{ code: "GA", name: "Gabon", flag: "🇬🇦" },
-	{ code: "GN", name: "Guinée", flag: "🇬🇳" },
 	{ code: "CD", name: "RDC", flag: "🇨🇩" },
+	{ code: "CF", name: "Centrafrique", flag: "🇨🇫" },
+	{ code: "TD", name: "Tchad", flag: "🇹🇩" },
+	{ code: "GM", name: "Gambie", flag: "🇬🇲" },
+	{ code: "GN", name: "Guinée Conakry", flag: "🇬🇳" },
+	{ code: "GW", name: "Guinée-Bissau", flag: "🇬🇼" },
 ];
 
 module.exports = countries;
